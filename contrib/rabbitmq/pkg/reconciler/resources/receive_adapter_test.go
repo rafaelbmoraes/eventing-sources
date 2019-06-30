januaryh/knative-eventing-sources

add Rabbitmq/amqp source

contrib/rabbitmq/pkg/reconciler/resources/receive_adapter_test.go
@@ -19,6 +19,7 @@ package resources
 import (
 	"github.com/google/go-cmp/cmp"
 	"github.com/knative/eventing-sources/contrib/rabbitmq/pkg/apis/sources/v1alpha1"
+	sourcesv1alpha1 "github.com/knative/eventing-sources/pkg/apis/sources/v1alpha1"
 	"k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -35,6 +36,22 @@ func TestMakeReceiveAdapter(t *testing.T) {
 			ServiceAccountName: "source-svc-acct",
 			Topic:              "topic",
 			Brokers:            "amqp://guest:guest@localhost:5672/",
+			User: sourcesv1alpha1.SecretValueFromSource{
+				SecretKeyRef: &corev1.SecretKeySelector{
+					LocalObjectReference: corev1.LocalObjectReference{
+						Name: "the-user-secret",
+					},
+					Key: "user",
+				},
+			},
+			Password: sourcesv1alpha1.SecretValueFromSource{
+				SecretKeyRef: &corev1.SecretKeySelector{
+					LocalObjectReference: corev1.LocalObjectReference{
+						Name: "the-password-secret",
+					},
+					Key: "password",
+				},
+			},
 			ExchangeConfig: v1alpha1.RabbitmqSourceExchangeConfigSpec{
 				Name:        "logs",
 				TypeOf:      "topic",
@@ -109,8 +126,38 @@ func TestMakeReceiveAdapter(t *testing.T) {
 									Value: "topic",
 								},
 								{
+									Name:      "RABBITMQ_USER",
+									ValueFrom: &corev1.EnvVarSource{
+										SecretKeyRef: &corev1.SecretKeySelector{
+											LocalObjectReference: corev1.LocalObjectReference{
+												Name: "the-user-secret",
+											},
+											Key: "user",
+										},
+									},
+								},
+								{
+									Name: "RABBITMQ_PASSWORD",
+									ValueFrom: &corev1.EnvVarSource{
+										SecretKeyRef: &corev1.SecretKeySelector{
+											LocalObjectReference: corev1.LocalObjectReference{
+												Name: "the-password-secret",
+											},
+											Key: "password",
+										},
+									},
+								},
+								{
 									Name:  "RABBITMQ_ROUTING_KEY",
 									Value: "*.critical",
+								},
+								{
+									Name:  "RABBITMQ_CHANNEL_CONFIG_PREFETCH_COUNT",
+									Value: "0",
+								},
+								{
+									Name:  "RABBITMQ_CHANNEL_CONFIG_QOS_GLOBAL",
+									Value: "false",
 								},
 								{
 									Name:  "RABBITMQ_EXCHANGE_CONFIG_NAME",

rafaelbmoraes/eventing-sources

contrib/rabbitmq/pkg/reconciler/resources/receive_adapter_test.go
@@ -18,7 +18,7 @@ package resources
 
 import (
 	"github.com/google/go-cmp/cmp"
-	"github.com/knative/eventing-sources/contrib/rabbitmq/pkg/apis/sources/v1alpha1"
+	"github.com/knative/eventing-contrib/contrib/rabbitmq/pkg/apis/sources/v1alpha1"
 	"k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -35,6 +35,22 @@ func TestMakeReceiveAdapter(t *testing.T) {
 			ServiceAccountName: "source-svc-acct",
 			Topic:              "topic",
 			Brokers:            "amqp://guest:guest@localhost:5672/",
+			User: v1alpha1.SecretValueFromSource{
+				SecretKeyRef: &corev1.SecretKeySelector{
+					LocalObjectReference: corev1.LocalObjectReference{
+						Name: "the-user-secret",
+					},
+					Key: "user",
+				},
+			},
+			Password: v1alpha1.SecretValueFromSource{
+				SecretKeyRef: &corev1.SecretKeySelector{
+					LocalObjectReference: corev1.LocalObjectReference{
+						Name: "the-password-secret",
+					},
+					Key: "password",
+				},
+			},
 			ExchangeConfig: v1alpha1.RabbitmqSourceExchangeConfigSpec{
 				Name:        "logs",
 				TypeOf:      "topic",
@@ -109,8 +125,38 @@ func TestMakeReceiveAdapter(t *testing.T) {
 									Value: "topic",
 								},
 								{
+									Name:      "RABBITMQ_USER",
+									ValueFrom: &corev1.EnvVarSource{
+										SecretKeyRef: &corev1.SecretKeySelector{
+											LocalObjectReference: corev1.LocalObjectReference{
+												Name: "the-user-secret",
+											},
+											Key: "user",
+										},
+									},
+								},
+								{
+									Name: "RABBITMQ_PASSWORD",
+									ValueFrom: &corev1.EnvVarSource{
+										SecretKeyRef: &corev1.SecretKeySelector{
+											LocalObjectReference: corev1.LocalObjectReference{
+												Name: "the-password-secret",
+											},
+											Key: "password",
+										},
+									},
+								},
+								{
 									Name:  "RABBITMQ_ROUTING_KEY",
 									Value: "*.critical",
+								},
+								{
+									Name:  "RABBITMQ_CHANNEL_CONFIG_PREFETCH_COUNT",
+									Value: "0",
+								},
+								{
+									Name:  "RABBITMQ_CHANNEL_CONFIG_QOS_GLOBAL",
+									Value: "false",
 								},
 								{
 									Name:  "RABBITMQ_EXCHANGE_CONFIG_NAME",